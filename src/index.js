c5ed841f860a9dbac3210fc7d17d8ad68704318e
Gonçalo Figueiredo <me@goncalovf.com>
2023-04-03 17:04:22 +0100

    Add tags to Twitter mention

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', e => {
   const twitterCTAs = document.getElementsByClassName('js-twitter-cta')
   if (twitterCTAs && twitterCTAs.length > 0) {
     const twitterCTA = twitterCTAs[0]
-    twitterCTA.setAttribute('href', twitterCTA.href + '%0a%0aID: ' + Math.random().toString(36).slice(2))
+    twitterCTA.setAttribute('href', twitterCTA.href.replace('whitelisting.', 'whitelisting.%0a%0aID: ' + Math.random().toString(36).slice(2)))
   }
 })
